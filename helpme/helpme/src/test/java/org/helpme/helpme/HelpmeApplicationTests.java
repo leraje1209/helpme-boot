e37a8e545f42b0e4148b961176b15af26a69e830
hyh920904@gmail.com <hyh920904@gmail.com>
2022-09-27 21:09:09 +0900

    0927 first commit

---
+++ helpme/helpme/src/test/java/org/helpme/helpme/HelpmeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.helpme.helpme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
